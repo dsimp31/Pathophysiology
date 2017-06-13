28c6a012bcd6d67cf19908c14b2bb0cde61b44f0
D. Simpson <dsimp31@wgu.edu>
2017-06-12 21:37:10 -0700

    Add project files.

---
+++ Pathophysiology/Classes/Disease.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+
+namespace Pathophysiology.Classes
+{
+    public class Disease
+    {
+        public Guid ID;         
+        public List<string> DiseaseNames;
+        public List<string> Specialties;        // pulmonary, infectious, etc.
+        public List<string> Relations;          // related diseases
+        public List<string> Symptoms;
+        public List<string> Causes;
+        public List<string> RiskFactors;
+        public List<string> Diagnostics;
+        public List<string> Treatments;
+    }
+}
---
+++ UnitTests/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pathophysiology.Classes;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CanLoadDiseaseXML()
+        {
+
+        }
+    }
+}
